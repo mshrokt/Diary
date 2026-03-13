2017a0aae6e587d51fd2ab56056901a45b61390d
mshrokt <mshrokt@gmail.com>
2026-03-13 20:59:14 +0900

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Turbopack conflict fix for the PWA wrapper
-  webpack: (config) => {
-    return config;
-  },
-};
+const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default nextConfig;
