6e9a9a19070caf67a370689caa36e3bcf5fedad1
mshrokt <mshrokt@gmail.com>
2026-03-14 11:34:32 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "diary-app-v1";
+const ASSETS = ["/"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
